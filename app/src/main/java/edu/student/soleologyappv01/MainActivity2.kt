25bb5b1e17e03fee1d3bcf45b3fbe666466707bd
RVCSupxjavi <s0360579@student.rockvalleycollege.edu>
2021-04-18 19:49:49 -0500

    Week 12

---
+++ app/src/main/java/edu/student/soleologyappv01/MainActivity2.kt
@@ -0,0 +1,12 @@
+package edu.student.soleologyappv01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+    }
+}
